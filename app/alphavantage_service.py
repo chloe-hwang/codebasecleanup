15b48c93a6f024aec918d05ada4bd263a125bc59
Chloe Hwang <98184096+chloe-hwang@users.noreply.github.com>
2022-04-07 10:37:09 -0400

    Testing (#8)
    * test price formatting logic
    
    * updated financial services
    
    * updated alphavantage_service.py

--- app/alphavantage_service.py
+++ app/alphavantage_service.py
@@ -6,7 +6,8 @@
 
 import os
 from dotenv import load_dotenv
-
+from app.utilities import to_usd
+from pandas import read_csv
 import requests
 import json
 
@@ -16,21 +17,22 @@ ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 
 
 def fetch_crypto_data(symbol):
-    # url = ...
-    # make a request
-    # return some data
-    return "TODO"
+    url = f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&market=USD&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+    response = requests.get(url)
+    parsed_response = json.loads(response.text)
+    return parsed_response["Time Series (Digital Currency Daily)"]
 
 
 def fetch_stocks_data(symbol):
-    # url = ...
-    # make a request
-    # return some data
-    return "TODO"
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
+
+    df = read_csv(url)
+    latest = df.iloc[0]
+    return latest
 
 
 def fetch_unemployment_data():
-    # url = ...
-    # make a request
-    # return some data
-    return "TODO"
+    url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={ALPHAVANTAGE_API_KEY}"
+    response = requests.get(url)
+    parsed_response = json.loads(response.text) 
+    return parsed_response
