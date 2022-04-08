229a54790e314f9307a8a75002f2015028b4e785
Chloe Hwang <ch1248@georgetown.edu>
2022-04-08 13:11:48 -0400

    unemployment and alphavantage updates

--- app/alphavantage_service.py
+++ app/alphavantage_service.py
@@ -47,4 +47,45 @@ def fetch_unemployment_data():
     url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={ALPHAVANTAGE_API_KEY}"
     response = requests.get(url)
     parsed_response = json.loads(response.text) 
-    return parsed_response
+    data = parsed_response["data"]
+    latest = data[0]
+    print(latest) #> {'date': '2022-02-01', 'value': '3.8'}
+    from pandas import DataFrame
+    from plotly.express import bar
+
+
+    df = DataFrame(data)
+    print(df.head())
+
+    fig = bar(df, x="date", y="value", title="Unemployment Rates")
+    # https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html
+
+    # https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html#plotly.graph_objects.Figure.update_yaxes
+    # https://plotly.com/python/reference/layout/yaxis/
+    # https://plotly.com/python/reference/layout/yaxis/#layout-yaxis-ticksuffix
+    fig.update_yaxes(
+        #tickprefix="$",
+        ticksuffix="%",
+        showgrid=True
+    )
+
+    fig.show()
+
+    #breakpoint()
+
+
+    print("DATAVIZ EXPORT...")
+     # https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html#plotly.graph_objects.Figure.to_image
+    # fig.to_image(format="png")
+
+    # https://plotly.com/python/static-image-export/
+    # Image export using the "kaleido" engine requires the kaleido package,
+    #which can be installed using pip:
+    #    $ pip install -U kaleido
+    img_filepath = os.path.join(os.path.dirname(__file__), "..", "reports", "unemployment.png")
+    fig.write_image(img_filepath)
+
+
+    print("CSV EXPORT...")
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "reports", "unemployment.csv")
+    df.to_csv(csv_filepath, index=False)
